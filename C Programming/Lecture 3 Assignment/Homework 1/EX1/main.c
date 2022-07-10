e6d2b4af7b9811324329a1759680d9aa6056a8a0
Omar Mekkawy <omar.k.mekkawy@gmail.com>
2022-07-10 13:33:35 +0200

    Added EX1 into the Homework 1

---
+++ C Programming/Lecture 3 Assignment/Homework 1/EX1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 10, 2022
+ *      Author: Omar Mekkawy
+ */
+
+// Include standard IO library
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("C Programming"); // print the sentence
+	return 0; // return no error
+}
