1a03ad2940357f992d4651c2a9ca62ef790e9385
Omar Mekkawy <omar.k.mekkawy@gmail.com>
2022-07-16 16:09:12 +0200

    Added main.c for Homework 3 EX1, README.md file for EX1, Output_EX1.png image

---
+++ C Programming/Lecture 4 Assignment/Arrays/EX1/README.md
@@ -0,0 +1,25 @@
+# Homework 3 - Arrays- EX1
+
+```
+Write a C program to find sum of two matrix of order 2*2 using multidimensional arrays where, elements of matrix are entered by user.
+
+output:
+Enter the elements of 1st matrix
+Enter a11: 2;
+Enter a12: 0.5;
+Enter a21: -1.1;
+Enter a22: 2;
+Enter the elements of 2nd matrix
+Enter b11: 0.2;
+Enter b12: 0;
+Enter b21: 0.23;
+Enter b22: 23;
+
+Sum Of Matrix:
+2.2     0.5
+-0.9    25.0
+```
+
+# Output
+
+<img src="./Output_EX1.png" width="1080" height="399">
---
+++ C Programming/Lecture 4 Assignment/Arrays/EX1/main.c
@@ -0,0 +1,59 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 11, 2022
+ *      Author: Omar Mekkawy
+ */
+
+// Include standard IO library
+#include <stdio.h>
+
+// Make the Matrix size configurable :D (Extra idea)
+#define Matrix_Number_Of_Rows 2
+#define Matrix_Number_Of_Columns 2
+
+float FirstMatrix[Matrix_Number_Of_Rows][Matrix_Number_Of_Columns];
+float SecondMatrix[Matrix_Number_Of_Rows][Matrix_Number_Of_Columns];
+
+int i,j;
+
+int main(void) {
+
+
+	printf("Enter the elements of 1st matrix\n");
+	for(i=0;i<Matrix_Number_Of_Rows;i++)
+	{
+		for(j=0;j<Matrix_Number_Of_Columns;j++)
+		{
+			printf("Enter a%d%d: ",i+1,j+1);
+			fflush(stdin);
+			fflush(stdout);
+			scanf("%f",&FirstMatrix[i][j]);
+		}
+	}
+
+	printf("Enter the elements of 2nd matrix\n");
+	for(i=0;i<Matrix_Number_Of_Rows;i++)
+	{
+		for(j=0;j<Matrix_Number_Of_Columns;j++)
+		{
+			printf("Enter b%d%d: ",i+1,j+1);
+			fflush(stdin);
+			fflush(stdout);
+			scanf("%f",&SecondMatrix[i][j]);
+		}
+	}
+
+	printf("Sum Of Matrix:\n");
+
+	for(i=0;i<Matrix_Number_Of_Rows;i++)
+	{
+		for(j=0;j<Matrix_Number_Of_Columns;j++)
+		{
+			printf("%.2f",FirstMatrix[i][j]+SecondMatrix[i][j]);
+			printf("\t");
+		}
+		printf("\n");
+	}
+
+}
